46a0c1ee3e6022d894d2169f7c5586d90f3c39da
panaganti priyanka <panagantipriyanka@gmail.com>
2021-11-30 16:51:53 +0530

    update

---
+++ recapcha.js
@@ -0,0 +1,8 @@
+window.onload=function () {
+  render();
+};
+function render() {
+    window.recaptchaVerifier=new firebase.auth.RecaptchaVerifier('recaptcha-container');
+    recaptchaVerifier.render();
+}
+
